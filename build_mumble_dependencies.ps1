f1c9747af251a4e0c62cdcffb1d8ac2cb192c148
Robert Adam <dev@robert-adam.de>
2023-12-29 10:09:23 +0100

    Fix powershell script

--- build_mumble_dependencies.ps1
+++ build_mumble_dependencies.ps1
@@ -1,29 +1,29 @@
 param (
 	[string]$TRIPLET = "x64-windows-static-md",
-	[string]$XCOMPILE_TRIPLET = "x86-windows-static-md"
-	[switch]$AUTO = ""
+	[string]$XCOMPILE_TRIPLET = "x86-windows-static-md",
+	[switch]$AUTO = $False
 )
 
 if (! $AUTO) {
-	// Make sure command-prompt stays open if an error is encountered
+	# Make sure command-prompt stays open if an error is encountered
 	trap { Read-Host "ERROR encountered... Press Enter to exit" }
 }
 
 $MUMBLE_DEPS = Get-Content -Path "$PSScriptRoot\mumble_dependencies.txt"
-// Windows-specific dependencies
-$MUMBLE_DEPS.Add("mdnsresponder")
-$MUMBLE_DEPS.Add("icu")
+# Windows-specific dependencies
+$MUMBLE_DEPS += "mdnsresponder"
+$MUMBLE_DEPS += "icu"
 
-if (![System.IO.File]::Exists("$PSScriptRoot\vcpkg.exe") {
+if (-Not [System.IO.File]::Exists("$PSScriptRoot\vcpkg.exe")) {
 	Start-Process -FilePath "$PSScriptRoot\bootstrap-vcpkg.bat" -ArgumentList @( "-disableMetrics" )
 }
 
 if ("$TRIPLET" -ne "$XCOMPILE_TRIPLET") {
 	Write-Host "Building xcompile dependencies..."
-	"$PSScriptRoot/vcpkg.exe" install --triplet "$XCOMPILE_TRIPLET" boost-optional --clean-after-build
+	& "$PSScriptRoot/vcpkg.exe" install --triplet "$XCOMPILE_TRIPLET" boost-optional --clean-after-build
 }
 
 foreach ($dep in $MUMBLE_DEPS) {
 	Write-Host "Building dependency $dep"
-	"$PSScriptRoot/vcpkg.exe" install --triplet "$TRIPLET" "$dep" --clean-after-build
+	& "$PSScriptRoot/vcpkg.exe" install --triplet "$TRIPLET" "$dep" --clean-after-build
 }

8c5e595083550359f4aeae8ff20ef704e9278303
Robert Adam <dev@robert-adam.de>
2025-08-16 09:26:59 +0100

    Ensure directory doesn't have trailing slash

--- compress_dir.sh
+++ compress_dir.sh
@@ -2,17 +2,24 @@
 
 set -e
 
-if [ -z "$1" ]; then
+input_dir="$1"
+
+if [[ -z "$input_dir" ]]; then
 	1>&2 echo "No input directory given"
 	exit 1
 fi
 
-if [ -n "$2" ]; then
+if [[ -n "$2" ]]; then
 	1>&2 echo "Too many arguments - expected only a single directory"
 	exit 2
 fi
 
-echo "Archiving $1 ..."
-tar -cf "$1.tar" "$1"
-echo "Compressing $1.tar ..."
-xz -9e "$1.tar"
+if [[ "$input_dir" = */ ]]; then
+	# Remove trailing slash
+	input_dir="${input_dir%/}"
+fi
+
+echo "Archiving $input_dir ..."
+tar -cf "$input_dir.tar" "$1"
+echo "Compressing $input_dir.tar ..."
+xz -9e "$input_dir.tar"

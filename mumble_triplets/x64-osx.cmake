791103b3397fd7ed41f74f802d63c427694c613c
Robert Adam <dev@robert-adam.de>
2026-08-16 17:38:20 +0100

    Bump OSX deployment target

--- mumble_triplets/x64-osx.cmake
+++ mumble_triplets/x64-osx.cmake
@@ -1,6 +1,8 @@
 # Inherit from this triplet
 include("${CMAKE_CURRENT_LIST_DIR}/../triplets/community/x64-osx-release.cmake")
 
-# Apparently Qt 6 doesn't support OSX 15 (yet)
 # Note: when changing this, you should also change the value for the arm64-osx triplet
-set(VCPKG_OSX_DEPLOYMENT_TARGET 13.0)
+# We can only use v15 or higher once we use a Qt version that includes
+# https://github.com/qt/qtbase/commit/6c12b7323dbc918d0720a9c4d96e4a2342681a6a
+# which will be 6.12+
+set(VCPKG_OSX_DEPLOYMENT_TARGET 14.0)
